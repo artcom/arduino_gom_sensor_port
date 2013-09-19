919692cfd84cd9c2f625f28d73a05afbaeb505c3
Martin Skinner <martin@artcom.de>
2013-09-19 13:15:52 +0200

    code cleanup, added net mask, changed port to unsigned int

--- simple_switch/simple_switch.ino
+++ simple_switch/simple_switch.ino
@@ -2,52 +2,73 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-byte mac[] = {0xAE, 0xAC, 0xBE, 0xEF, 0xFE, 0xEB};
+byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x0D, 0xCE};
 EthernetUDP udp;
 
 // destination IP
-byte gspIp[] = {169,254,76,3};
+byte gspIp[] = {172,40,2,22};
 // own IP in network
-byte myIp[] = {169,254,76,4};
+byte myIp[] = {
+  172,40,13,31}; // 172,40,13,30
+
+byte myDns[] = {8,8,8,8}; 
+byte myGateway[] = {172,40,1,1}; 
+byte myNetmask[] = {255,255,0,0}; 
+
 // UDP port
-int gspPort = 12346;
-int lastValue = -1;
+unsigned int gspPort = 44470;
+
+const int inputPin = 8;  
+int pVal = HIGH;
 
 void setup() {
-	Serial.begin(9600);
-        Serial.println("try to initialize Ethernet");
-        Ethernet.begin(mac, myIp);
-	if(0 != 0) {
-		Serial.println("Failed to configure Ethernet using DHCP");
-	} else {
-		Serial.println("start udp session");
-		udp.begin(4221);
-		delay(1000);
-	}
-        Serial.println(Ethernet.localIP());
+    Serial.begin(9600);
+    Serial.println("try to initialize Ethernet");
+    Ethernet.begin(mac, myIp, myDns, myGateway, myNetmask);
+    if(0 != 0) {
+        Serial.println("Failed to configure Ethernet using DHCP");
+    } 
+    else {
+        Serial.println("start udp session");
+        udp.begin(4221);
+        delay(1000);
+    }
+
+    Serial.println(Ethernet.localIP());
+
+    pinMode(inputPin, INPUT);
+    digitalWrite(inputPin,HIGH); 
+
 }
 
 void loop() {	
-	int value = analogRead(A0);
-        value = map(value, 0, 1023, 0, 5);
-        value = map(value, 0, 5, 0, 255);
-    
-        if(value != lastValue) {
-		lastValue = value;		
-		String test = "508:";
-		test += value;
-		char msg[30];
-		test.toCharArray(msg, 30);
-		Serial.println(msg);
-		writeToGSP(msg);
-	}
+    int val = digitalRead(inputPin);  // read input value
+    String test;
+
+    if(val != pVal){
+        if (val == HIGH){                  
+            test = "lightswitch:on";
+        }
+        else{
+            test = "lightswitch:off";	
+        }
+        char msg[30];
+        test.toCharArray(msg, 30);
+        writeToGSP(msg);
+    }
+
+    pVal = val; 
 }
 
 void writeToGSP(char* msg) {
-	udp.beginPacket(gspIp, gspPort);
-	udp.write(msg);
-	udp.endPacket();
-        Serial.print("sent: ");
-        Serial.print(msg);
-        Serial.println(" as udp package");
+    udp.beginPacket(gspIp, gspPort);
+    udp.write(msg);
+    udp.endPacket();
+    Serial.print("sent: ");
+    Serial.print(msg);
+    Serial.println(" as udp package");
 }
+
+
+
+
